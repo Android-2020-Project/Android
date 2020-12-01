d19a4d1d3f250de9039bf9f4a5de48cd9a17212e
Stonepreheim <Stonepreheim1@gmail.com>
2020-12-01 13:24:13 -0600

    added adapter for searching users
    button still not functional need to parse users correctly could use an extra pair of eyes on this

---
+++ app/src/main/java/com/example/parstagram_android/controllers/SearchAdapter.java
@@ -0,0 +1,78 @@
+package com.example.parstagram_android.controllers;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.parstagram_android.R;
+import com.example.parstagram_android.models.User;
+
+import java.util.List;
+
+public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder> {
+    private List<User> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    // data is passed into the constructor
+    public SearchAdapter(Context context, List<User> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.searchresult, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        User user = mData.get(position);
+        holder.myTextView.setText(user.getUser().getUsername());
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData == null ? 0 : mData.size();
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView myTextView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.searchEntry);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // convenience method for getting data at click position
+    User getItem(int id) {
+        return mData.get(id);
+    }
+
+    // allows clicks events to be caught
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
--- app/src/main/java/com/example/parstagram_android/views/fragments/SearchFragment.java
+++ app/src/main/java/com/example/parstagram_android/views/fragments/SearchFragment.java
@@ -5,15 +5,32 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.parstagram_android.R;
+import com.example.parstagram_android.controllers.SearchAdapter;
+import com.example.parstagram_android.models.User;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 
 public class SearchFragment extends Fragment {
+    private RecyclerView searchrv;
+    protected SearchAdapter adapter;
+    protected List<User> allUsers;
+    protected EditText userET;
+    protected Button searchButton;
+
 
     public SearchFragment() {
         // Required empty public constructor
@@ -30,5 +47,30 @@ public class SearchFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        searchButton = view.findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                searchUsers(v);
+            }
+        });
+        userET = view.findViewById(R.id.searchET);
+        searchrv = view.findViewById(R.id.searchrv);
+        adapter = new SearchAdapter(getContext(), allUsers);
+        searchrv.setAdapter(adapter);
+        searchrv.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    public void searchUsers(View v){
+        ParseQuery<User> query = ParseQuery.getQuery(User.class);
+        query.whereContains("username", userET.getText().toString());
+        query.findInBackground(new FindCallback<User>() {
+            @Override
+            public void done(List<User> users, ParseException e) {
+                for (User user: users){
+                }
+                allUsers.addAll(users);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
